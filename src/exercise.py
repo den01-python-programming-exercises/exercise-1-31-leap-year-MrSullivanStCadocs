687336ec54d6dde08a535e5a1c8ecf691fbccbec
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 13:16:12 +0000

    chaned 4 to 400 in first condition

--- src/exercise.py
+++ src/exercise.py
@@ -2,7 +2,7 @@ def main():
     #write your code below this line
     year = int(input("Give a year:"))
 
-    if (year%100==0 and year%4==0):
+    if (year%100==0 and year%400==0):
       print("The year is a leap year.")
     elif((year % 4 == 0)):
       print("The year is a leap year.")
