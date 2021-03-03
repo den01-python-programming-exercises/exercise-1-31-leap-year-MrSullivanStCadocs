1ce5053292bfc4e4e7b1a325165402603a8f032b
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 13:18:53 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -2,9 +2,7 @@ def main():
     #write your code below this line
     year = int(input("Give a year:"))
 
-    if (year%100==0 and year%400==0):
-      print("The year is a leap year.")
-    elif((year % 4 == 0)):
+    if (year%4==0):
       print("The year is a leap year.")
     else:
       print("The year is not a leap year.")
