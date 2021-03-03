1e3b5d97218241b820231084366a8827b4783764
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 13:52:31 +0000

    head work

--- src/exercise.py
+++ src/exercise.py
@@ -6,10 +6,12 @@ def main():
       print("The year is not a leap year.")
     elif (year%100==0 and year%400==0):
       print("The year is a leap year")
+    elif(year%100==0):
+      print("The year is not a leap year.")
     elif(year%4==0):
       print("The year is a leap year.")
     else:
-      print("The year is not a leap year.")
+      print("This year is not a leap year.")
 
 if __name__ == '__main__':
     main()
