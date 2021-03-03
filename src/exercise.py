4b465c720964bebb07a1fff1f642f097d9cd5eda
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 13:39:19 +0000

    sstatement

--- src/exercise.py
+++ src/exercise.py
@@ -4,7 +4,11 @@ def main():
 
     if (year%4!=0):
       print("The year is not a leap year.")
+    elif (year%100==0 and year%400==0):
+      print("The year is a leap year")
     elif(year%4==0):
+      print("The year is a leap year.")
+    else:
       print("The year is not a leap year.")
 
 if __name__ == '__main__':
