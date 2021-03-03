e32a28aed777cd776184dd4303f3e5ef917278e7
MrSullivan <rsullivan@stcadocs.com>
2021-03-03 13:29:44 +0000

    not equal to and equal to statements

--- src/exercise.py
+++ src/exercise.py
@@ -2,10 +2,10 @@ def main():
     #write your code below this line
     year = int(input("Give a year:"))
 
-    if (year%4==0):
-      print("The year is a leap year.")
-    else:
+    if (year%4!=0):
       print("The year is not a leap year.")
+    elif(year%4==0):
+      print("The year is a leap year.")
 
 if __name__ == '__main__':
     main()
